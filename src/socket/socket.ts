7a55b108636af53381ab373cd93416be96676078
My-Name-Is-Rafael-Sampaio <rafaelsampaiodejesus.30@gmail.com>
2024-11-06 16:23:29 -0300

    modified socket.ts

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,5 +1,6 @@
 import { Server as HttpServer } from "http";
 import { Server, Namespace } from "socket.io";
+// import jwt, { JwtPayload } from "jsonwebtoken";
 import handleGpsEvents from "./events/gpsEvents";
 
 const initializeSocket = (server: HttpServer) => {
@@ -12,17 +13,40 @@ const initializeSocket = (server: HttpServer) => {
 
   const gpsNamespace: Namespace = io.of("/gps");
 
+  // gpsNamespace.use((socket, next) => {
+  //   const token = socket.handshake.auth.token;
+  //   const origin = socket.handshake.headers.origin;
+
+  //   if (origin === "https://wokwi.com") {
+  //     return next();
+  //   }
+
+  //   if (!token) {
+  //     return next(new Error("Acesso negado. Token não fornecido."));
+  //   }
+
+  //   jwt.verify(
+  //     token,
+  //     "YOUR_SECRET_KEY",
+  //     (err: Error | null, decoded: string | JwtPayload | undefined) => {
+  //       if (err) {
+  //         return next(new Error("Falha na autenticação do token."));
+  //       }
+  //       (socket as any).user = decoded;
+  //       next();
+  //     }
+  //   );
+  // });
+
   gpsNamespace.on("connection", (socket) => {
-    // console.log(`Client connected to namespace /gps: ${socket.id}`);
+    console.log(`Client connected to namespace /gps: ${socket.id}`);
 
     handleGpsEvents(socket, gpsNamespace);
 
     socket.on("disconnect", () => {
-      // console.log(`Client disconnected from namespace /gps: ${socket.id}`);
+      console.log(`Client disconnected from namespace /gps: ${socket.id}`);
     });
   });
-
-  return io;
 };
 
 export default initializeSocket;
